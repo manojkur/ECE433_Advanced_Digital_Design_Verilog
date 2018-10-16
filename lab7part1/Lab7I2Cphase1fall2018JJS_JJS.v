b1bd82af65c2b1f06d335b6d8de8d2191ff979f1
LeelaPakanati <41307800+LeelaPakanati@users.noreply.github.com>
2018-10-16 15:02:53 -0400

    modified top level and added baud rate

--- lab7part1/Lab7I2Cphase1fall2018JJS_JJS.v
+++ lab7part1/Lab7I2Cphase1fall2018JJS_JJS.v
@@ -10,32 +10,42 @@
 
 module Lab7I2Cphase1fall2018MK_LP(Go, SCL, SDA, Reset, clock, ClockLocked);
 
-input Go, Reset, clock;
+input Go, Reset, clock_input;
 output SCL, ClockLocked;
 inout SDA;
 //send this byte as address
 parameter FirstByte=8'b10010001;
 //30kHz I2C speed at 60MHz clock
 //parameter BaudRate=20'd30000, ClockFrequency=30'd60000000;
-wire clk60MHz;
+wire clock;
 //These are simulation parameters. 
 //Comment the following two lines before making bit stream file
-parameter BaudRate=2, ClockFrequency=20;
+parameter BaudRate=30000, ClockFrequency=60000000;
 //assign clk60MHz=clock;
 //disable Clock60MHz to simulate this circuit
-Clock60MHz SystemClock(clock,clk60MHz,ClockLocked);
+Clock60MHz SystemClock(
+	.CLK_IN1(clock_input),
+	.CLK_OUT1(clock),
+	.LOCKED(ClockLocked));
 
 wire WriteLoad, ReadorWrite, ShiftorHold, Select, BaudEnable, StartStopAck;
 //module SquareWaveGenerator (WaveFrequency, ClockFrequency, StartStop, SignalOut, Reset, clock);
-I2C_BaudRateGenerator  BaudUnit(BaudRate, ClockFrequency, BaudEnable, SCL, Reset, clk60MHz);
+I2C_BaudRateGenerator  BaudUnit(
+	.uartClock(SCL), 
+	.Reset(Reset),
+	.clock(clock),
+	.enable(enable),
+	.BaudRate(BaudRate),
+	.ClockFrequency(ClockFrequency));
+
 
 I2C_Controller  ControlUnit(Go, WriteLoad, ReadorWrite, ShiftorHold, Select, 
-	BaudEnable, StartStopAck, Reset, SCL, clk60MHz);
+	BaudEnable, StartStopAck, Reset, SCL, clock);
 
 //module I2C_DataUnit (WriteLoad, ReadorWrite, ShiftorHold, Select, SentData, 
 //ReceivedData, SDA, StartStopAck, Reset, clock);
 wire [7:0] ReceivedData;
 I2C_DataUnit DataUnit(WriteLoad, ReadorWrite, ShiftorHold, Select, FirstByte, 
-ReceivedData, SDA, StartStopAck, Reset, clk60MHz);
+ReceivedData, SDA, StartStopAck, Reset, clock);
 
 endmodule
