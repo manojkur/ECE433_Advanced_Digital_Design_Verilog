b61bba13a538ef7530e581cf9bba8185e20092a9
LeelaPakanati <41307800+LeelaPakanati@users.noreply.github.com>
2018-10-16 16:02:07 -0400

    did morre shi:

--- lab7part1/ControlUnit.v
+++ lab7part1/ControlUnit.v
@@ -16,13 +16,55 @@ module ControlUnit (
 	parameter TransitState=3'd5, StopState=3'd6; 
 
 
+	reg ClearTimer;
+	reg [2:0] NextState;
+	reg [3:0] DataCounter;
+
+	reg TimeOut;
 
 	always @(posedge clock, posedge reset) begin
 		if(reset) begin
-			
+			State <= InitialState;
 		end else begin
-			
+			State <= NextState;
 		end
+	end
+
+	always @(State, Go, ClockI2C, DataCounter, TimeOut)begin
+		case(State)
+			InitialState: begin
+				BaudEnable <= 0;
+				ReadorWrite <= 0;
+				WriteLoad <= 0;
+				Select <= 0;				
+				ShiftorHold <= 0;
+				StartStopAck <= 0;
+				Count <= 2;
+				ClearTimer <= 1;
+				if(Go)
+					if(ClockI2C)
+						NextState <= StartState;
+					else
+						NextState <= InitialState;
+				else 
+					NextState <= InitialState;
+			end
+
+			StartState:
+
+			LoadState:
+
+			WriteState:
+
+			AcknowledgeState:
+
+			TransitState:
+
+			StopState:
+
+			default:
+
+		endcase // State
 	end
 
 
