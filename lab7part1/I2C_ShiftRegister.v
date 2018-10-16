ee7a81a048cf363d2cdabcf23b1141c502624c69
manojkur <manoj.kurapati@gmail.com>
2018-10-16 15:38:28 -0400

    added Shift Register

--- lab7part1/I2C_ShiftRegister.v
+++ lab7part1/I2C_ShiftRegister.v
@@ -8,7 +8,32 @@ module I2C_ShiftRegister(WriteLoad, SentData, ReceivedData,ShiftIn,ShiftOut,Shif
 
 input CLOCK, Reset, ShiftIn, ShiftorHold, WriteLoad;
 input [7:0] SentData;
-output ShiftOut;
-output [7:0] RecievedData;
+output reg ShiftOut;
+output reg [7:0] RecievedData;
+
+reg [7:0] newRecievedData;
+
+always @(posedge CLOCK or posedge Reset)
+	if(Reset)
+		begin
+			ShiftOut <= 0;
+			RecievedData <= 8'b00000000;
+		end
+	else
+		begin
+			ShiftOut <= RecievedData[7];
+			ReceivedData <= newReceivedData;
+		end
+	
+always @(ShiftorLoad or WriteLoad)
+	if(WriteLoad)
+		ReceivedData <= SentData;
+	else
+		begin
+			if(ShiftorLoad)
+				newRecievedData <= {RecievedData[6:0], ShiftIn};
+			else
+				newRecievedData <= RecievedData;
+		end
 
 endmodule
