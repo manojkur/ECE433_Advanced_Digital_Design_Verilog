6edaff8fc72e1169abcf659a9abb6000a4c19e62
Leela Pakanati <leela.pakanati@gmail.com>
2018-10-22 15:33:43 -0400

    test

--- lab7part1/ControlUnit.v
+++ lab7part1/ControlUnit.v
@@ -11,6 +11,7 @@ module ControlUnit (
 	output WriteLoad	
 );
 
+
 	reg [2:0] State;
 	parameter InitialState=3'd0, StartState=3'd1, LoadState=3'd2, WriteState=3'd3, AcknowledgeState=3'd4;
 	parameter TransitState=3'd5, StopState=3'd6; 
