0ee946eedb8c09bdfa142bf1a8aa49a20870acb9
Leela Pakanati <leela.pakanati@gmail.com>
2018-10-22 15:52:24 -0400

    sda module made; data unit and top level modified

--- lab7part1/I2C_DataUnit.v
+++ lab7part1/I2C_DataUnit.v
@@ -4,18 +4,37 @@
 //Date: October 16, 2018
 //ECE433 Fall 2018
 //Phase 1 of Lab #7 I2C driver and TMP101 temperature sensor
-module I2C_DataUnit (WriteLoad, ReadorWrite, ShiftorHold, Select, SentData,  
-ReceivedData, SDA, StartStopAck, Reset, clock); 
+module I2C_DataUnit (
+	input WriteLoad, 
+	input ReadorWrite, 
+	input ShiftorHold, 
+	input Select, 
+	input [Length-1:0] SentData,
+	output [length-1:0] ReceivedData,
+	inout SDA,
+	input StartStopAck,
+	input Reset,
+	input clock); 
+
 parameter LENGTH = 8; 
-input WriteLoad, ReadorWrite, ShiftorHold, Select, StartStopAck; 
-inout SDA; 
-input Reset, clock; 
-input [LENGTH-1:0]   SentData; 
-output [LENGTH-1:0]  ReceivedData; 
 //module I2C_ShiftRegister(WriteLoad, SentData, ReceivedData,ShiftIn,ShiftOut,ShiftorHold,Reset,CLOCK);
 wire ShiftDataIn, ShiftDataOut; 
-I2C_ShiftRegister ShiftUnit(WriteLoad, SentData, ReceivedData,ShiftDataIn, ShiftDataOut,ShiftorHold, 
-Reset,clock);
+I2C_ShiftRegister ShiftUnit(
+	.(WriteLoad), 
+	.(SentData), 
+	.(ReceivedData),
+	.(ShiftDataIn), 
+	.(ShiftDataOut),
+	.(ShiftorHold),
+	.Reset(Reset),
+	.clock(clock));
+
 //module I2C_SDAmodule(SDA, ReadorWrite, Select, StartStopAck, ShiftIn, ShiftOut);
-I2C_SDAmodule SDAUnit(SDA, ReadorWrite, Select, StartStopAck, ShiftDataIn, ShiftDataOut);
+I2C_SDAmodule SDAUnit(
+	.(SDA), 
+	.(ReadorWrite), 
+	.(Select), 
+	.(StartStopAck), 
+	.(ShiftDataIn), 
+	.(ShiftDataOut));
 endmodule 
--- lab7part1/Lab7I2Cphase1fall2018JJS_JJS.v
+++ lab7part1/Lab7I2Cphase1fall2018JJS_JJS.v
@@ -8,25 +8,30 @@
 //Output: 
 //Go is "1" to start communication
 
-module Lab7I2Cphase1fall2018MK_LP(Go, SCL, SDA, Reset, clock, ClockLocked);
+module Lab7I2Cphase1fall2018MK_LP(
+	input Go,
+	inout SCL,
+	output SDA,
+	input Reset,
+	input clock_input,
+	output ClockLocked);
 
-input Go, Reset, clock_input;
-output SCL, ClockLocked;
-inout SDA;
 //send this byte as address
 parameter FirstByte=8'b10010001;
+
 //30kHz I2C speed at 60MHz clock
 //parameter BaudRate=20'd30000, ClockFrequency=30'd60000000;
 wire clock;
+
 //These are simulation parameters. 
 //Comment the following two lines before making bit stream file
 parameter BaudRate=30000, ClockFrequency=60000000;
-//assign clk60MHz=clock;
-//disable Clock60MHz to simulate this circuit
-Clock60MHz SystemClock(
-	.CLK_IN1(clock_input),
-	.CLK_OUT1(clock),
-	.LOCKED(ClockLocked));
+
+assign clock = clock_input
+// Clock60MHz SystemClock(
+// 	.CLK_IN1(clock_input),
+// 	.CLK_OUT1(clock),
+// 	.LOCKED(ClockLocked));
 
 wire WriteLoad, ReadorWrite, ShiftorHold, Select, BaudEnable, StartStopAck;
 //module SquareWaveGenerator (WaveFrequency, ClockFrequency, StartStop, SignalOut, Reset, clock);
@@ -54,7 +59,9 @@ ControlUnit  ControlUnit(
 //module I2C_DataUnit (WriteLoad, ReadorWrite, ShiftorHold, Select, SentData, 
 //ReceivedData, SDA, StartStopAck, Reset, clock);
 wire [7:0] ReceivedData;
-I2C_DataUnit DataUnit(WriteLoad, ReadorWrite, ShiftorHold, Select, FirstByte, 
+I2C_DataUnit DataUnit(
+	.(WriteLoad),
+	.(ReadorWrite), ShiftorHold, Select, FirstByte, 
 ReceivedData, SDA, StartStopAck, Reset, clock);
 
 
